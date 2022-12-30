f24e492ab6d7c4ab79384b509de29780f3516543
ritvik130 <rjoha091@uottawa.ca>
2022-12-29 19:30:00 -0500

    Initialized DB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 5000;
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 const app = express();
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
